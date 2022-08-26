36ce20d6e14b7471eaed8e14d0889457b7a110c9
Hadi Zare <hadizareoriginal@gmail.com>
2022-08-26 17:29:07 +0430

    Revert "fix(routes): hard code path"
    This reverts commit 524a3abb4e2ce2016585648f450c4a61c859d9a1.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import HomePage from "./Pages/Home";
 import QuestionPage from "./Pages/Question";
 import {Routes, Route} from "react-router-dom";
+import * as routes from "./Shared/Constants/routes";
 import NotFoundPage from "./Pages/404";
 
 const App = () => {
@@ -9,7 +10,7 @@ const App = () => {
         <Routes>
             <Route path={'/'}>
                 <Route index element={<HomePage/>}/>
-                <Route path={'question'}>
+                <Route path={routes.QUESTION_DETAILS_PAGE}>
                     <Route path=':id' element={<QuestionPage/>}/>
                 </Route>
                 <Route index element={<NotFoundPage/>}/>
