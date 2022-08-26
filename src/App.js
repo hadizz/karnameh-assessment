524a3abb4e2ce2016585648f450c4a61c859d9a1
Hadi Zare <hadizareoriginal@gmail.com>
2022-08-26 17:25:22 +0430

    fix(routes): hard code path

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import HomePage from "./Pages/Home";
 import QuestionPage from "./Pages/Question";
 import {Routes, Route} from "react-router-dom";
-import * as routes from "./Shared/Constants/routes";
 import NotFoundPage from "./Pages/404";
 
 const App = () => {
@@ -10,7 +9,7 @@ const App = () => {
         <Routes>
             <Route path={'/'}>
                 <Route index element={<HomePage/>}/>
-                <Route path={routes.QUESTION_DETAILS_PAGE}>
+                <Route path={'question'}>
                     <Route path=':id' element={<QuestionPage/>}/>
                 </Route>
                 <Route index element={<NotFoundPage/>}/>
