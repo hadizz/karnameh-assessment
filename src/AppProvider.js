67a33799d3c761cbe238ac2d457dfd1b45927a8f
Hadi Zare <hadizareoriginal@gmail.com>
2022-08-26 17:12:26 +0430

    fix(routes): hopefully fix base url issues in gh page and add 404 pages

--- src/AppProvider.js
+++ src/AppProvider.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import {BrowserRouter} from 'react-router-dom';
+import {BrowserRouter as Router} from "react-router-dom";
+import {appBaseUrl} from "./Shared/Constants/routes";
 
 const AppProvider = ({children}) => {
     return (
-        <BrowserRouter>
+        <Router basename={appBaseUrl}>
             {children}
-        </BrowserRouter>
+        </Router>
     );
 };
 
